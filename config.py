b3b2474c103bdaf882f29bf5e12e7757cc7da1ed
sammmmmm099 <srnvsxig@gmail.com>
2025-09-10 15:49:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "24473318"
-api_hash = "e7dd0576c5ac0ff8f90971d6bb04c8f5"
-bot_token = "8034069514:AAHUBpzSCq41jPwsJkDbXuEoVC_yCxzyuw0"
-auth_users = [5840594311]
+api_id = "25259066"
+api_hash = "caad2cdad2fe06057f2bf8f8a8e58950"
+bot_token = "8426712421:AAH7yV6TWR4zRcWAQqKrOirKlshaUl6uTig"
+auth_users = [6421294389]
